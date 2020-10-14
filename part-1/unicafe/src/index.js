95206cdcb6742eb2b5de36d519469162424e9552
Sehroz Khan <info@sehroz.com>
2020-10-13 22:21:42 -0400

    1.8: unicafe step3

--- part-1/unicafe/src/index.js
+++ part-1/unicafe/src/index.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Stats = ({ good, bad, neutral }) => {
+  const all = good + neutral + bad;
+  const avg = all > 0 ? (good + bad * -1) / all : 0;
+  const pos = all > 0 ? (good / all) * 100 : 0;
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {avg}</p>
+      <p>positive {pos} %</p>
+    </>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-  const avg = all > 0 ? (good + bad * -1) / all : 0;
-  const pos = all > 0 ? (good / all) * 100 : 0;
-
-  const handleVote = (setVote) => () => {
+  const handleVote = ({ setVote }) => () => {
     return setVote === "good"
       ? setGood(good + 1)
       : setVote === "neutral"
@@ -29,13 +43,7 @@ const App = () => {
         <button onClick={handleVote("bad")}>bad</button>
       </div>
       <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {avg}</p>
-        <p>positive {pos} %</p>
+        <Stats good={good} bad={bad} neutral={neutral} />
       </div>
     </>
   );
