e8e3ae5f1e9f25ab6c851e0fd9ac8c8ff4ded307
Sehroz Khan <info@sehroz.com>
2020-10-14 05:43:41 -0400

    2.2: Course information step7 & 2.3*: Course information step8

--- part-2/course-information/src/component/Total.js
+++ part-2/course-information/src/component/Total.js
@@ -1,10 +1,8 @@
 import React from "react";
 
 const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
+  const sum = course.parts.reduce((a, b) => a + b.exercises, 0);
+
   return <p>Number of exercises {sum}</p>;
 };
 
