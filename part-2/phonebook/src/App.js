51a803da322d17272b40bbe19c8db56af13cca92
Sehroz Khan <info@sehroz.com>
2020-10-14 23:38:38 -0400

    2.9*: The Phonebook Step4

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterInput, setFilterInput] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -15,6 +21,8 @@ const App = () => {
     persons.find((person) => person.name === newName)
       ? alert(`${newName} is already added to phonebook`)
       : setPersons([...persons, newPerson]);
+
+    setNewNumber("");
   };
 
   const handleInput = (e) => {
@@ -25,15 +33,30 @@ const App = () => {
       : null;
   };
 
+  const handleFilter = (e) => {
+    setFilterInput(e.target.value);
+  };
+
+  const list =
+    filterInput.length > 0
+      ? persons.filter((person) =>
+          person.name.toLowerCase().includes(filterInput.toLowerCase())
+        )
+      : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterInput} onChange={handleFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleInput} name="name" />
         </div>
         <div>
-          number:{" "}
+          number:
           <input value={newNumber} onChange={handleInput} name="number" />
         </div>
         <div>
@@ -41,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {list.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
