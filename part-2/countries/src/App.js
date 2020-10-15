a28a601e13526523055e57804382dd1c3f02bae8
Sehroz Khan <info@sehroz.com>
2020-10-15 08:55:30 -0400

    2.13*: Data for countries, step2

--- part-2/countries/src/App.js
+++ part-2/countries/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ShowCountries = ({ countries, search }) => {
+  const [show, setShow] = useState(false);
+  const [showCountry, setShowCountry] = useState("");
   const list = countries.filter((country) =>
     country.name.toLowerCase().includes(search.toLowerCase())
   );
@@ -11,11 +13,31 @@ const ShowCountries = ({ countries, search }) => {
     return message;
   }
 
+  const handleShow = (e) => {
+    setShow(!show);
+    setShowCountry(e.target.name);
+  };
+
   if (list.length <= 10 && list.length > 1) {
     return (
       <>
         {list.map((country) => (
-          <p key={country.name}>{country.name}</p>
+          <div key={country.name}>
+            {country.name}
+
+            {show && country.name === showCountry ? (
+              <>
+                <button onClick={handleShow} name={country.name}>
+                  close
+                </button>
+                <CountryData country={country} />
+              </>
+            ) : (
+              <button onClick={handleShow} name={country.name}>
+                show
+              </button>
+            )}
+          </div>
         ))}
       </>
     );
@@ -24,26 +46,32 @@ const ShowCountries = ({ countries, search }) => {
   return (
     <>
       {list.map((country) => (
-        <div key={country.name}>
-          <h1> {country.name} </h1>
-          <p>captial {country.capital}</p>
-          <p>population {country.population}</p>
-          <h2>languages</h2>
-          {country.languages.map((lang) => (
-            <ul key={lang.name}>
-              <li> {lang.name} </li>
-            </ul>
-          ))}
-          <div>
-            <img
-              src={country.flag}
-              alt={country.name}
-              style={{ height: "100px", width: "auto" }}
-            />
-          </div>
-        </div>
+        <CountryData country={country} />
       ))}
     </>
+  );
+};
+
+const CountryData = ({ country }) => {
+  return (
+    <div key={country.name}>
+      <h1> {country.name} </h1>
+      <p>captial {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>languages</h2>
+      {country.languages.map((lang) => (
+        <ul key={lang.name}>
+          <li> {lang.name} </li>
+        </ul>
+      ))}
+      <div>
+        <img
+          src={country.flag}
+          alt={country.name}
+          style={{ height: "100px", width: "auto" }}
+        />
+      </div>
+    </div>
   );
 };
 
