056f4210667060a82264abf3dfa37aa127377496
Sehroz Khan <info@sehroz.com>
2020-10-15 07:23:01 -0400

    2.11: The Phonebook Step6

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./component/Filter";
 import PersonForm from "./component/PersonForm";
 import Persons from "./component/Persons";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterInput, setFilterInput] = useState("");
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  }, []);
   const addPerson = (e) => {
     e.preventDefault();
     const newPerson = {
