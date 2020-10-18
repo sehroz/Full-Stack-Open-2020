bf274e1781764e346ade99a93cb52de5e560d4dc
Sehroz Khan <info@sehroz.com>
2020-10-17 22:31:13 -0400

    3.3: Phonebook backend step3

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 3001;
@@ -13,6 +14,17 @@ app.get("/info", (req, res) => {
   const mesage = `<p>Phonebook has info or ${length} people</p>
                   <p>${datetime}</p>`;
   res.send(mesage);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = peopleDB.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 app.listen(PORT, () => {
   console.log(`Server on:${PORT}`);
