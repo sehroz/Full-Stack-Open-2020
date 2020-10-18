fb3f343d3a96df8f496a5ea983b7846a3c9bfd22
Sehroz Khan <info@sehroz.com>
2020-10-17 22:50:17 -0400

    3.4: Phonebook backend step4

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -2,14 +2,35 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 3001;
-const peopleDB = require("./db.json");
+let peopleDB = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
 
 app.get("/api/persons", (req, res) => {
-  res.send(peopleDB);
+  res.json(peopleDB);
 });
 
 app.get("/info", (req, res) => {
-  const length = Object.keys(peopleDB).length;
+  const length = peopleDB.length;
   const datetime = new Date();
   const mesage = `<p>Phonebook has info or ${length} people</p>
                   <p>${datetime}</p>`;
@@ -25,6 +46,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  peopleDB = peopleDB.filter((person) => person.id !== id);
+
+  res.status(204).end();
 });
 app.listen(PORT, () => {
   console.log(`Server on:${PORT}`);
