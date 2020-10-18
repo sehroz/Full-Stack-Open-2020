d7e59f70c0c8a19f612da1cce96bb50201de754f
Sehroz Khan <info@sehroz.com>
2020-10-17 23:11:21 -0400

    3.6: Phonebook backend step6

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -66,6 +66,12 @@ const generateID = () => {
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "content missing" });
+  } else if (peopleDB.find((person) => person.name == body.name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
   const person = {
     name: body.name,
     number: body.number,
