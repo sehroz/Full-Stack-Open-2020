a4cc92b44b6f64c32d83c301edbfa15ee186ab94
Sehroz Khan <info@sehroz.com>
2020-10-19 03:15:44 -0400

    3.13: Phonebook database, step1

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,36 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const Person = require("./models/person");
+const PORT = process.env.PORT;
 
 morgan.token("data", function getId(req) {
   const data = JSON.stringify(req.body);
   return data;
 });
-
-let peopleDB = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
 
 app.use(
   cors(),
@@ -39,26 +18,15 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 app.get("/api/persons", (req, res) => {
-  res.json(peopleDB);
-});
-
-app.get("/info", (req, res) => {
-  const length = peopleDB.length;
-  const datetime = new Date();
-  const mesage = `<p>Phonebook has info or ${length} people</p>
-                  <p>${datetime}</p>`;
-  res.send(mesage);
+  Person.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = peopleDB.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id).then((people) => {
+    res.json(people);
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -67,12 +35,6 @@ app.delete("/api/persons/:id", (req, res) => {
 
   res.status(204).end();
 });
-
-const generateID = () => {
-  const newId = Math.floor(Math.random() * 1000000);
-
-  return newId;
-};
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
@@ -83,15 +45,14 @@ app.post("/api/persons", (req, res) => {
     return res.status(400).json({ error: "name must be unique" });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateID(),
-  };
+  });
 
-  peopleDB = peopleDB.concat(person);
-
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 app.listen(PORT, () => {
