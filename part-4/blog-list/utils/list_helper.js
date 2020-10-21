1bac529d1aac6af555deee28cb7eb206138048b5
Sehroz Khan <info@sehroz.com>
2020-10-21 13:05:20 -0400

    4.4: helper functions and unit tests, step2

--- part-4/blog-list/utils/list_helper.js
+++ part-4/blog-list/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((sum, item) => {
+    return sum + item['likes']
+  }, 0)
+
+  return likes
+}
 module.exports = {
   dummy,
+  totalLikes,
 }
