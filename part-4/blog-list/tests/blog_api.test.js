72ef0e8934cd5338e8b20b9728a6c85ee6e2b66b
Sehroz Khan <info@sehroz.com>
2020-10-22 13:58:00 -0400

    4.9*: Blog list tests, step2

--- part-4/blog-list/tests/blog_api.test.js
+++ part-4/blog-list/tests/blog_api.test.js
@@ -1,14 +1,46 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const blogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(blogs[0])
+  await blogObject.save()
+  blogObject = new Blog(blogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier property of the blog posts is named id,', async () => {
+  const blogs = await api.get('/api/blogs')
+
+  expect(blogs.body[0]['id']).toBeDefined()
 })
 
 afterAll(() => {
