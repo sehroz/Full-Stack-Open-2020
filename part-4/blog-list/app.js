e632f997b16338fe80e820bda4b93c7d4b044c7f
Sehroz Khan <info@sehroz.com>
2020-10-23 16:28:13 -0400

    4.18: bloglist expansion, step6

--- part-4/blog-list/app.js
+++ part-4/blog-list/app.js
@@ -7,6 +7,8 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
@@ -25,6 +27,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part-4/blog-list/controllers/blogs.js
+++ part-4/blog-list/controllers/blogs.js
@@ -1,16 +1,31 @@
+require('express-async-errors')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const user = await User.findById(request.body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: request.body.title,
