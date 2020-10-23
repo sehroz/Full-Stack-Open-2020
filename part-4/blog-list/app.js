ab7c6446617fe6fab52e27feb8032e514828f547
Sehroz Khan <info@sehroz.com>
2020-10-23 17:40:34 -0400

    4.20*: bloglist expansion, step8

--- part-4/blog-list/app.js
+++ part-4/blog-list/app.js
@@ -25,6 +25,7 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
