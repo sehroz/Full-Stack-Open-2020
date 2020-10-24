80e3c47d5dd7ab5fab7c447a9a2e43f90ac961c6
Sehroz Khan <info@sehroz.com>
2020-10-24 18:34:23 -0400

    5.4*: bloglist frontend, step4

--- part-5/bloglist-frontend/src/App.js
+++ part-5/bloglist-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Msg from './components/Message'
 
 const App = () => {
   const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
@@ -9,9 +10,16 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [msg, setMsg] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
+    blogService
+      .getAll()
+      .then((blogs) => setBlogs(blogs))
+      .catch((err) => {
+        setMsg({ msg: `Did not get people list: ${err}`, type: 'success' })
+        setTimeout(() => setMsg(null), 5000)
+      })
   }, [])
 
   useEffect(() => {
@@ -32,16 +40,29 @@ const App = () => {
       })
 
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
-      blogService.setToken(user.Token)
+      blogService.setToken(user.token)
       setUser(user)
+      setMsg({
+        msg: `Logged In`,
+        type: 'success',
+      })
+      setTimeout(() => setMsg(null), 5000)
       setUsername('')
       setPassword('')
-    } catch (exception) {}
+    } catch (error) {
+      setMsg({ msg: error.response.data.error, type: 'fail' })
+      setTimeout(() => setMsg(null), 5000)
+    }
   }
 
   const handleLogout = () => {
     window.localStorage.clear()
     setUser(null)
+    setMsg({
+      msg: `Logged Out`,
+      type: 'success',
+    })
+    setTimeout(() => setMsg(null), 5000)
   }
 
   const handleBlogChange = (e) => {
@@ -72,7 +93,7 @@ const App = () => {
     </form>
   )
 
-  const addBlog = (event) => {
+  const addBlog = async (event) => {
     event.preventDefault()
     const blogObject = {
       title: newBlog.title,
@@ -80,10 +101,15 @@ const App = () => {
       url: newBlog.url,
     }
 
-    blogService.create(blogObject).then((returnedBlog) => {
-      setBlogs(blogs.concat(returnedBlog))
-      setNewBlog({ title: '', author: '', url: '' })
+    const returnedBlog = await blogService.create(blogObject)
+
+    setBlogs(blogs.concat(returnedBlog))
+    setMsg({
+      msg: `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`,
+      type: 'success',
     })
+    setTimeout(() => setMsg(null), 5000)
+    setNewBlog({ title: '', author: '', url: '' })
   }
 
   const blogForm = () => (
@@ -97,20 +123,23 @@ const App = () => {
     </form>
   )
 
-  if (user === null) {
-    return <div>{loginForm()}</div>
-  }
-
   return (
     <div>
       <h2>blogs</h2>
-      <h3>
-        {username} logged in <button onClick={handleLogout}>logout</button>
-      </h3>
-      {blogForm()}
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      <Msg msg={msg} />
+      {user ? (
+        <>
+          <h3>
+            {username} logged in <button onClick={handleLogout}>logout</button>
+          </h3>
+          {blogForm()}
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
+      ) : (
+        <>{loginForm()}</>
+      )}
     </div>
   )
 }
