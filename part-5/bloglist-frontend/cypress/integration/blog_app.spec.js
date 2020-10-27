338df824442b3380d82b13af92b9390f37ddd5c8
Sehroz Khan <info@sehroz.com>
2020-10-27 15:17:18 -0400

    5.18: bloglist end to end testing, step2

--- part-5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part-5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -2,6 +2,12 @@ describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
+    const user = {
+      name: 'Sehroz',
+      username: 'sehroz',
+      password: '12345',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
   })
 
   it('Login form is shown', function () {
@@ -9,5 +15,23 @@ describe('Blog app', function () {
     cy.get('#password')
     cy.get('#login-button')
     cy.contains('login').click()
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('sehroz')
+      cy.get('#password').type('12345')
+      cy.get('#login-button').click()
+
+      cy.contains('Logged In')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('abcd')
+      cy.get('#password').type('abcd')
+      cy.get('#login-button').click()
+
+      cy.contains('invalid username or password')
+    })
   })
 })
