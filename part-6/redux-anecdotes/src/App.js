a447eee6ca4732cf9dfb30f62880279d24c41071
Sehroz Khan <info@sehroz.com>
2020-10-30 22:30:28 -0400

    6.13 Anecdotes and the backend, step1

--- part-6/redux-anecdotes/src/App.js
+++ part-6/redux-anecdotes/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecService from './services/anecs'
+import { initializeAnecs } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecService.getAll().then((anecs) => dispatch(initializeAnecs(anecs)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -16,8 +7,6 @@ const asObject = (anecdote) => {
     votes: 0,
   }
 }
-
-const initialState = anecdotesAtStart.map(asObject)
 
 export const addVote = (id) => {
   return {
@@ -34,12 +23,21 @@ export const addAnec = (anecWords) => {
   }
 }
 
-const anecReducer = (state = initialState, action) => {
+export const initializeAnecs = (anecs) => {
+  return {
+    type: 'INIT_ANECS',
+    data: anecs,
+  }
+}
+
+const anecReducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch (action.type) {
     case 'ADD_ANEC':
       return state.concat(action.data)
+    case 'INIT_ANECS':
+      return action.data
     case 'ADD_VOTE':
       const id = action.data.id
       const anecToVote = state.find((anec) => anec.id === id)
