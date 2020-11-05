0802a1c5c36694706362184b3388d445a5ec6660
Sehroz Khan <info@sehroz.com>
2020-11-04 22:34:01 -0500

    7.15: Blog view

--- part-7/bloglist/frontend/src/components/SingleBlog.js
+++ part-7/bloglist/frontend/src/components/SingleBlog.js
@@ -6,19 +6,20 @@ export const SingleBlog = ({ blog, user, handleLike, deleteBlog }) => {
   }
   return (
     <div>
-      <div> {blog.url} </div>
+      <h1>{blog.title}</h1>
+      <a href={blog.url}> {blog.url} </a>
       <div className='likes'>
-        {blog.likes}
+        {blog.likes} likes
         <button id={'like' + blog.title} onClick={() => handleLike(blog.id)}>
           like
         </button>
-
         {blog.user.username === user.username ? (
           <button id='deleteBlogButton' onClick={() => deleteBlog(blog.id)}>
             remove
           </button>
         ) : null}
       </div>
+      <p>added by {blog.author}</p>
     </div>
   )
 }
