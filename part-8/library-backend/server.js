7f7f48195de0ef42537130df74f748012184d474
Sehroz Khan <info@sehroz.com>
2020-11-06 17:11:02 -0500

    8.2: All books

--- part-8/library-backend/server.js
+++ part-8/library-backend/server.js
@@ -79,9 +79,19 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    city: String!
+    genres: [String]
+    id: ID!
+  }
+
   type Query {
     authorCount: Int!
     bookCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -89,6 +99,7 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
+    allBooks: () => books,
   },
 }
 
