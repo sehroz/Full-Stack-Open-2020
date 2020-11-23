7698d22860bb46545144c7b4ced6e7bd14418761
Sehroz Khan <info@sehroz.com>
2020-11-23 00:10:36 -0500

    9.9: Patientor backend, step2

--- part-9/patientor/server/index.ts
+++ part-9/patientor/server/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import cors from "cors";
 const app = express();
-app.use(express.json());
+app.use(cors(), express.json());
 
-const PORT = 3000;
+const PORT = 3002;
 
 app.get("/ping", (_req, res) => {
   res.send("Hey");
